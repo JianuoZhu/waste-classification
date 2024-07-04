c442eed77969ff9ba70ca91b94bf2b09c98461bd
JianuoZhu <jianuo.zhu@gmail.com>
2024-07-04 11:53:45 +0800

    infer

--- inference.py
+++ inference.py
@@ -3,7 +3,7 @@ import pandas as pd
 import torch
 from torchvision import transforms
 from model import ClsHead, create_model, count_parameters
-import os
+import time
 
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -12,7 +12,7 @@ def main():
     model = create_model().to(device)
     model.load_state_dict(torch.load('best_model.pth'))
     print("Total number of trainable parameters:", count_parameters(model))
-    img_path = 'dataset_resized/metal/metal1.jpg'
+    img_path = 'dataset-resized/metal/metal1.jpg'
     image = Image.open(img_path).convert('RGB')
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
@@ -20,8 +20,12 @@ def main():
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
     image = transform(image)
+    image = image.unsqueeze(0)
     image = image.to(device)
     output = model(image)
+    output = output.logits
     _, predicted = output.max(1)
     print(f"the predicted class is: {predicted}")
-    os.sleep(5)
+    time.sleep(5)
+
+main()
